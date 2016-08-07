c94f1e5a2c621b6091f2e121a2b86c85090324db
orasmiyya <resmiyye.o@code.edu.az>
2016-08-07 08:53:15 +0400

    Bagcam

---
+++ js/bagha.js
@@ -0,0 +1,11 @@
+   
+    $(document).ready(function() {
+        $('.slider2').bxSlider({
+            slideWidth: 300,
+            minSlides: 2,
+            maxSlides: 4,
+            slideMargin: 10,
+            pager: false
+        });
+    });
+   
